4f086d781383424871c88258351aab0cc30c2e2a
Kevin Jolley <serrowpls@gmail.com>
2018-06-21 16:14:03 -0500

    updated graph

--- src/graph.py
+++ src/graph.py
@@ -19,7 +19,6 @@ class Graph:
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=140, y=140)
         debug_vertex_3 = Vertex('t3', x=300, y=400)
-        debug_vertex_4 = Vertex('t4', x=400, y=100)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
